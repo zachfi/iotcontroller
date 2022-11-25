50138b89f27b4bacd4d7f68bfdb757a406862b28
Zach Leslie <xaque208@gmail.com>
2022-11-25 04:10:37 +0000

    Include otel tracing

--- main.go
+++ main.go
@@ -16,11 +16,13 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"log/slog"
 	"os"
+	"time"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
