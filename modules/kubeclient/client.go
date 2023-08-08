7874eb7f7e4a2581d37b5b357d697c5761f96389
Zach Leslie <xaque208@gmail.com>
2023-08-08 00:12:00 +0000

    Kubeclient changes in flux

--- modules/kubeclient/client.go
+++ modules/kubeclient/client.go
@@ -5,10 +5,14 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/grafana/dskit/services"
-	iotv1 "github.com/zachfi/iotcontroller/api/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	iotv1 "github.com/zachfi/iotcontroller/api/v1"
 )
 
 type KubeClient struct {
@@ -36,21 +40,22 @@ func New(cfg Config, logger log.Logger) (*KubeClient, error) {
 		return nil, err
 	}
 
-	// iotConfig := *config
-	// iotConfig.GroupVersion = &schema.GroupVersion{
-	// 	Group:   iotv1.GroupVersion.Group,
-	// 	Version: iotv1.GroupVersion.Version,
-	// }
-	// iotConfig.APIPath = "/apis"
-	// iotConfig.NegotiatedSerializer = serializer.NewCodecFactory(scheme.Scheme)
-	// iotConfig.UserAgent = rest.DefaultKubernetesUserAgent()
+	iotConfig := *config
+	iotConfig.GroupVersion = &schema.GroupVersion{
+		Group:   iotv1.GroupVersion.Group,
+		Version: iotv1.GroupVersion.Version,
+	}
+	iotConfig.APIPath = "/apis"
+	iotConfig.NegotiatedSerializer = serializer.NewCodecFactory(scheme.Scheme)
+	iotConfig.UserAgent = rest.DefaultKubernetesUserAgent()
 
+	// TODO: Using global scheme.  runtime.New() would also provide a specific scheme.
 	err = iotv1.AddToScheme(scheme.Scheme)
 	if err != nil {
 		return nil, err
 	}
 
-	// exampleRestClient, err := rest.UnversionedRESTClientFor(&crdConfig)
+	// iotClient, err := rest.UnversionedRESTClientFor(&iotConfig)
 	// if err != nil {
 	// 	return nil, err
 	// }
@@ -76,8 +81,10 @@ func New(cfg Config, logger log.Logger) (*KubeClient, error) {
 	// 	return nil, err
 	// }
 
-	// k.client = clientset
+	// k.clientset = clientset
 	k.client = client
+
+	// k.store = WatchResource(clientSet)
 
 	return k, nil
 }
@@ -106,3 +113,22 @@ func (k *KubeClient) stopping(_ error) error {
 func (k *KubeClient) run(ctx context.Context) error {
 	return nil
 }
+
+// func WatchResources(clientSet client_v1alpha1.ExampleV1Alpha1Interface) cache.Store {
+// 	projectStore, projectController := cache.NewInformer(
+// 		&cache.ListWatch{
+// 			ListFunc: func(lo metav1.ListOptions) (result runtime.Object, err error) {
+// 				return clientSet.Projects("some-namespace").List(lo)
+// 			},
+// 			WatchFunc: func(lo metav1.ListOptions) (watch.Interface, error) {
+// 				return clientSet.Projects("some-namespace").Watch(lo)
+// 			},
+// 		},
+// 		&v1alpha1.Project{},
+// 		1*time.Minute,
+// 		cache.ResourceEventHandlerFuncs{},
+// 	)
+//
+// 	go projectController.Run(wait.NeverStop)
+// 	return projectStore
+// }
