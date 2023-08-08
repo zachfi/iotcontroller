fe001ca8f29b786f73689eb755e78aea35e1c9a7
Zach Leslie <xaque208@gmail.com>
2023-08-08 00:08:42 +0000

    Update config

---
+++ modules/conditioner/conditioner.go
@@ -0,0 +1,62 @@
+package conditioner
+
+import (
+	"context"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/grafana/dskit/services"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/zachfi/iotcontroller/modules/kubeclient"
+	"github.com/zachfi/iotcontroller/modules/mqttclient"
+	iotv1 "github.com/zachfi/iotcontroller/proto/iot/v1"
+)
+
+const (
+	namespace = "iot"
+)
+
+type Conditioner struct {
+	services.Service
+	cfg *Config
+
+	logger log.Logger
+	tracer trace.Tracer
+
+	klient     *kubeclient.KubeClient
+	mqttClient *mqttclient.MQTTClient
+}
+
+func New(cfg Config, logger log.Logger, mqttClient *mqttclient.MQTTClient, kubeclient *kubeclient.KubeClient) (*Conditioner, error) {
+	c := &Conditioner{
+		cfg:        &cfg,
+		logger:     log.With(logger, "module", "conditioner"),
+		tracer:     otel.Tracer("conditioner"),
+		klient:     kubeclient,
+		mqttClient: mqttClient,
+	}
+
+	c.Service = services.NewBasicService(c.starting, c.running, c.stopping)
+
+	return c, nil
+}
+
+func (c *Conditioner) Alert(ctx context.Context, req *iotv1.AlertRequest) (*iotv1.AlertResponse, error) {
+	_ = level.Info(c.logger).Log("msg", "alert received")
+	return &iotv1.AlertResponse{}, nil
+}
+
+func (c *Conditioner) starting(ctx context.Context) error {
+	return nil
+}
+
+func (c *Conditioner) running(ctx context.Context) error {
+	<-ctx.Done()
+	return nil
+}
+
+func (c *Conditioner) stopping(_ error) error {
+	return nil
+}
