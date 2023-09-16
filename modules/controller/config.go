d045002314d60c8a9cb496887435c52764f41588
Zach Leslie <xaque208@gmail.com>
2023-09-16 22:22:10 +0000

    Namespace the controller

--- modules/controller/config.go
+++ modules/controller/config.go
@@ -10,11 +10,13 @@ type Config struct {
 	MetricsAddr          string `yaml:"metrics_addr"`
 	ProbeAddr            string `yaml:"probe_addr"`
 	EnableLeaderElection bool   `yaml:"enable_leader_election"`
+	Namespace            string `yaml:"namespace"`
 }
 
 func (cfg *Config) RegisterFlagsAndApplyDefaults(prefix string, f *flag.FlagSet) {
 	f.StringVar(&cfg.MetricsAddr, util.PrefixConfig(prefix, "metrics-bind-address"), ":8090", "The address the metric endpoint binds to.")
 	f.StringVar(&cfg.ProbeAddr, util.PrefixConfig(prefix, "health-probe-bind-address"), ":8091", "The address the probe endpoint binds to.")
+	f.StringVar(&cfg.Namespace, util.PrefixConfig(prefix, "namespace"), "iot", "The namespace for this controller to operate within")
 	f.BoolVar(&cfg.EnableLeaderElection, util.PrefixConfig(prefix, "leader-elect"), false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
--- modules/controller/controller.go
+++ modules/controller/controller.go
@@ -61,6 +61,7 @@ func New(cfg Config, logger *slog.Logger) (*Controller, error) {
 		HealthProbeBindAddress: c.cfg.ProbeAddr,
 		LeaderElection:         c.cfg.EnableLeaderElection,
 		LeaderElectionID:       "cefdf353.iot",
+		Namespace:              cfg.Namespace,
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
