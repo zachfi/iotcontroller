1704e382b12db9048239da27a6c9e8045778785a
Zach Leslie <xaque208@gmail.com>
2024-02-22 14:00:55 +0000

    Fixes for schedule handling and cleanup

--- modules/conditioner/schedule.go
+++ modules/conditioner/schedule.go
@@ -71,7 +71,7 @@ func (s *schedule) add(ctx context.Context, name string, t time.Time, req reques
 	defer s.Unlock()
 
 	if v, ok := s.events[name]; ok {
-		if matched(v.req, req) {
+		if matched(v.req, req) && t.Equal(v.t) {
 			return
 		}
 
@@ -101,6 +101,25 @@ func (s *schedule) add(ctx context.Context, name string, t time.Time, req reques
 	}(ctx, req)
 }
 
+// Any names not listed in the map are removed from the event.  This allows us
+// to clean up the currenting runnign schedules when the backing condition has
+// been removed or renamed.
+func (s *schedule) removeExtraneous(names map[string]struct{}) {
+	s.Lock()
+	defer s.Unlock()
+
+	var namesToDelete []string
+	for k := range s.events {
+		if v, ok := names[k]; !ok {
+			namesToDelete = append(namesToDelete, k)
+		}
+	}
+
+	for _, n := range namesToDelete {
+		delete(s.events, n)
+	}
+}
+
 // run calls the client for each request
 func (s *schedule) run(ctx context.Context, client iotv1proto.ZoneKeeperServiceClient) {
 	for {
@@ -123,4 +142,37 @@ func (s *schedule) run(ctx context.Context, client iotv1proto.ZoneKeeperServiceC
 			}
 		}
 	}
+}
+
+type scheduleStatus struct {
+	Name  string
+	Next  string
+	Scene string
+	State string
+}
+
+func (s *schedule) Status() []scheduleStatus {
+	s.Lock()
+	defer s.Unlock()
+
+	var stati []scheduleStatus
+
+	for n, e := range s.events {
+		ss := scheduleStatus{
+			Name: n,
+			Next: e.t.Format(time.RFC3339),
+		}
+
+		if e.req.sceneReq != nil {
+			ss.Scene = e.req.sceneReq.Scene
+		}
+
+		if e.req.stateReq != nil {
+			ss.State = e.req.stateReq.State.String()
+		}
+
+		stati = append(stati, ss)
+	}
+
+	return stati
 }
